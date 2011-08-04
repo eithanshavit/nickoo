e2146c7566dd48e81bb2c3165060e055d16ab4a8
Eithan <oogiyot@gmail.com>
2011-08-04 20:25:33 +0300

    Create App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nickoo::Application.config.secret_token = '3f8af256ad67af51e88ae827c652ae2c2cc26b70e5c416f90d4a5f761679d4577ea5d2cec6b90f448ecb3720f86f85a8e14333ed72cf2e15cf2762a918336824'
